b4a6bf484964cf8f5dd5d193cdece129d4393f9a
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-08-25 12:46:16 +0300

    feat(experience): add experience data types

---
+++ src/types/experience.ts
@@ -0,0 +1,16 @@
+export interface ExperienceData {
+    company_name: string;
+    company_url?: string | null;
+    position: string;
+    Employment_type?: string | null;
+    location?: string | null;
+    description?: string | null;
+    start_date: string;
+    end_date?: string | null;
+    is_current: boolean;
+    display_order: number;
+}
+
+export type CreateExperienceData = ExperienceData;
+
+export type UpdateExperienceData = Partial<ExperienceData>;
