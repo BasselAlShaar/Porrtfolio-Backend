80bc22be20b03e5f299b8cfa46278c3977bde52d
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-08-28 01:12:33 +0300

    feat(skills_category): add skills_category service

---
+++ src/https/services/skills/skills_category.service.ts
@@ -0,0 +1,48 @@
+import skills_categoryRepository from "../../repositories/skills/skills_category.repository.js";
+
+import type {
+    CreateSkillsCategoryData,
+    UpdateSkillsCategoryData
+} from "../../../types/skill_category.js"
+
+//get all
+const getAllEducations = async () => {
+    return skills_categoryRepository.find_all();
+};
+
+//create
+const createSkillCategory = async (data: CreateSkillsCategoryData) => {
+    return skills_categoryRepository.create(data);
+}
+
+//find by id
+const findSkillCategoryById = async (id: string) => {
+    return skills_categoryRepository.findById(id);
+}
+
+//update
+const updateSkillCategory = async (
+    id: string,
+    data: UpdateSkillsCategoryData
+) => {
+    const existingSkillCategory =
+        await skills_categoryRepository.findById(id);
+    
+    if (!existingSkillCategory) {
+        return null;
+    }
+
+    return skills_categoryRepository.update(id, data);
+};
+
+//delete
+const deleteSkillCategory = async (id: string) => {
+
+    const existingSkillCategory = await skills_categoryRepository.findById(id);
+
+    if(!existingSkillCategory) {
+        return null;
+    }
+
+    return skills_categoryRepository.delete(id);
+}
