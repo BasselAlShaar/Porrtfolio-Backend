aa34cb2d564d27c81ea995fde930bdf1776fa4cc
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-08-30 20:42:19 +0300

    feat(technologies): add technologies types

---
+++ src/types/technologies/technologies.ts
@@ -0,0 +1,10 @@
+export interface TechnologiesData {
+    name : string;
+    description? : string | null;
+    icon? : string | null;
+    display_order : number;
+}
+
+export type CreateTechnologiesData = TechnologiesData;
+
+export type UpdateTechnologiesData = Partial<TechnologiesData>;
