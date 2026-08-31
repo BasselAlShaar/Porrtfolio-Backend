1f58db0262ee5acfdf71346210c3a3417d005752
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-09-01 01:10:53 +0300

    feat(education_achievements): add education achievements service

--- src/https/repositories/education/education_achievements.repository.ts
+++ src/https/repositories/education/education_achievements.repository.ts
@@ -123,3 +123,11 @@ const remove = async (id: string) => {
 
     return result.rows[0] ?? null;
 }
+
+export default {
+    find_all,
+    getById,
+    create,
+    update,
+    delete: remove
+}
---
+++ src/https/services/education/education_achievements.service.ts
@@ -0,0 +1,57 @@
+import education_achievementsRepository from "../../repositories/education/education_achievements.repository.js";
+
+import type { 
+    CreateEducationAchievementsData,
+    UpdateEducationAchievementsData
+} from "../../../types/education_achievements.js";
+
+//get all
+const getAllEducationAchievements = async (id: string) => {
+    return education_achievementsRepository.find_all(id);
+}
+
+//get one
+const getEducationAchievementById = async (id: string) => {
+    return education_achievementsRepository.getById(id);
+}
+
+//create
+const createEducationAchievement = async (
+    id: string,
+    data: CreateEducationAchievementsData
+) => {
+    return education_achievementsRepository.create(id,data);
+}
+
+//update
+const updateEducationAchievement = async (
+    id: string,
+    data: UpdateEducationAchievementsData
+) => {
+    const result = await education_achievementsRepository.getById(id);
+
+    if (!result) {
+        return null;
+    }
+
+    return education_achievementsRepository.update(id, data);
+}
+
+//delete
+const deleteEducationAchievement = async (id: string) => {
+    const result = await education_achievementsRepository.getById(id);
+
+    if (!result) {
+        return null;
+    }
+
+    return education_achievementsRepository.delete(id);
+}
+
+export default {
+    getAllEducationAchievements,
+    getEducationAchievementById,
+    createEducationAchievement,
+    updateEducationAchievement,
+    deleteEducationAchievement
+}
