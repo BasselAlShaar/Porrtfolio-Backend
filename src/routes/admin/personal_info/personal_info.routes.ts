ee2a8532bc3340d12c8828abe1787048feece021
Bassel Al Shaar <basselalshaar11@gmail.com>
2026-09-01 16:05:23 +0300

    feat(personal_info): add personal info routes

--- src/routes/admin/personal_info/personal_info.routes.ts
+++ src/routes/admin/personal_info/personal_info.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import socialLinksRouter from "./social_links.routes.js";
+import personal_infoController from "../../../https/controllers/personal_info/personal_info.controller.js";
+import { validateUpdate } from "../../../https/middlewares/personal_info/personal_info.middleware.js";
 
 const profileRouter = Router();
 
 profileRouter.use("/links", socialLinksRouter);
 
 //Get Profile
-profileRouter.get('/', );
+profileRouter.get('/', personal_infoController.getPersonalInfo);
 
 //Update Profile
-profileRouter.patch('/', );
+profileRouter.patch('/', validateUpdate, personal_infoController.updatePersonalInfo);
 
 export default profileRouter;
